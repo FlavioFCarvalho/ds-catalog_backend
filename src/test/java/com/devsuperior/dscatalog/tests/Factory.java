61658d57481146f28b63e841caa916e2fa3d7af9
FlavioFCarvalho <flaviocfr@gmail.com>
2022-01-03 21:02:22 -0300

    Test save with id null.

---
+++ src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "PC Gamer Foo", "Good Gamer",4170.0, "https://img.com/img.png",
+				Instant.parse("2020-07-14T10:00:00Z"));
+		product.getCategories().add(new Category(2L, "Eletronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+
+}
