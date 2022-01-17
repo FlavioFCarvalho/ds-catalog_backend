af808d7f085c0bc422ea469d70806444952a1409
FlavioFCarvalho <flaviocfr@gmail.com>
2022-01-17 09:55:43 -0300

    Teste de integração para findAllPaged

--- src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 	
 	@Autowired
@@ -28,7 +34,46 @@ public class ProductServiceIT {
 		existingID = 1L;
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
-	}	
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		
+		PageRequest papePageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(papePageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenDoesNotExist() {
+		
+		PageRequest papePageRequest = PageRequest.of(80, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(papePageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+		
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+		
+		PageRequest papePageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(papePageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+		
+	}
 	
 	@Test
 	public void deleteShouldDeleteResouceWhenIdExists() {
